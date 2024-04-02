c82d3d46507356b3b37abeda1c0b0d3f7e85f7eb
otabekpanoev01 <otabekpanoev01@gmail.com>
2024-04-02 20:59:02 +0500

    jar lesson finish

--- src/main/java/B.java
+++ src/main/java/B.java
@@ -1,7 +1,7 @@
-public class A {
+public class B {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 100; i++) {
-            System.out.println(i + " : A");
+            System.out.println(i + " : B");
             Thread.sleep(100);
         }
     }
