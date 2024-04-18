47bbff48f18f37a13fe087128ae17c5cc643b426
otabekpanoev01 <otabekpanoev01@gmail.com>
2024-04-18 17:26:32 +0500

    smth

--- src/main/java/uz/pdp/UrlAndUrlConnection.java
+++ src/main/java/uz/pdp/UrlAndUrlConnection.java
@@ -12,19 +12,19 @@ public class UrlAndUrlConnection {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
-        URL url = new URL("https://kun.uz");  // API
-        URLConnection urlConnection = url.openConnection();
-        InputStream inputStream = urlConnection.getInputStream();
-//        byte[] bytes = inputStream.readAllBytes();
-//        String string = new String(bytes);
-//        System.out.println(string);
-
-        Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
-
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
-            Thread.sleep(100);
-        }
+//        URL url = new URL("https://kun.uz");  // API
+//        URLConnection urlConnection = url.openConnection();
+//        InputStream inputStream = urlConnection.getInputStream();
+////        byte[] bytes = inputStream.readAllBytes();
+////        String string = new String(bytes);
+////        System.out.println(string);
+//
+//        Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
+//
+//        while (scanner.hasNext()) {
+//            System.out.println(scanner.nextLine());
+//            Thread.sleep(100);
+//        }
 
     }
 }
